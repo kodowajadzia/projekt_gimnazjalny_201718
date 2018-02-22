326913e4aeee0de6ce9fb8fcf02e8c8c242bb0fd
Maciej Kowalski <kowalski.exe@gmail.com>
2018-02-22 17:29:12 +0100

    Enemy Spawner

--- Jadwigowy TD/Assets/Jadwigowy TD/Scripts/Attack Path/Signpost.cs
+++ Jadwigowy TD/Assets/Jadwigowy TD/Scripts/Attack Path/Signpost.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class Signpost : MonoBehaviour {
 
+    public const string START_SIGNPOST_TAG = "Start Signpost", END_SIGNPOST_TAG = "End Signpost";
+
     public Signpost nextSignpost;
 
 #if UNITY_EDITOR
     public bool showPath = true;
 
-	void Update () {
+    private void Start() {
+        if (nextSignpost == null && gameObject.tag != END_SIGNPOST_TAG)
+            Debug.LogError("If the signpost is the lost one it should has a " + END_SIGNPOST_TAG + " tag.");
+    }
+
+    void Update () {
         if (showPath) {
             if (nextSignpost != null)
                 Debug.DrawLine(transform.position, nextSignpost.transform.position, Color.red);
@@ -19,11 +26,11 @@ public class Signpost : MonoBehaviour {
 	}
 
     private void OnDrawGizmos() {
-        if (tag == "Start Signpost") {
+        if (tag == START_SIGNPOST_TAG) {
             Vector3 offset = new Vector3(0, 1);
             Handles.Label(transform.position + offset, "Start");
         }
-        if(tag == "End Signpost") {
+        if(tag == END_SIGNPOST_TAG) {
             Vector3 offset = new Vector3(0, 1);
             Handles.Label(transform.position + offset, "End");
         }
---
+++ Jadwigowy TD/Assets/Jadwigowy TD/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyType{
+    ToTests,
+};
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject[] enemysPrefabs;
+
+#if UNITY_EDITOR
+    private void Awake() {
+        CheckEnumAndTablic();
+        CheckNullsInTablic();
+    }
+
+    private void CheckEnumAndTablic() {
+        int enemysEnumLenght = Enum.GetNames(typeof(EnemyType)).Length;
+        Debug.Assert(enemysEnumLenght == enemysPrefabs.Length, "Enum EnemyType should respond with the tablic of prefabs.");
+    }
+
+    private void CheckNullsInTablic() {
+        foreach (GameObject gameObject in enemysPrefabs)
+            Debug.Assert(gameObject != null, "A null enemy?");
+    }
+#endif
+
+    public void Spawn(EnemyType type) {
+        GameObject ins = Instantiate(enemysPrefabs[(int)type]);
+        ins.transform.position = transform.position;
+    }
+}
