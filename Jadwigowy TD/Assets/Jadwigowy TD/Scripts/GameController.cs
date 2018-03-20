119fce21222475ac525433d07b53cc77ea963bde
Maciej Kowalski <kowalski.exe@gmail.com>
2018-03-20 20:20:03 +0100

    dlaczego nikt nie ogarnia gita

--- Jadwigowy TD/Assets/Jadwigowy TD/Scripts/GameController.cs
+++ Jadwigowy TD/Assets/Jadwigowy TD/Scripts/GameController.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
     [SerializeField] private int maxPasses = 5;
     private int passes;
+
     private float money = 100;
     public float Money { get { return money; } set { money = value; } }
+
+    private int score;//score awarding
+
+    public const string currency = "BTC";
+
+    [SerializeField] private Text moneyText, scoreText;
 
     public void Pass() {
         passes++;
@@ -15,7 +23,12 @@ public class GameController : MonoBehaviour {
             Lose();
     }
 
-    private void Lose() {
+    public void Lose() {
         Debug.Log("game over");
+    }
+
+    private void OnGUI() {
+        moneyText.text = Money + " " + currency;
+        scoreText.text = score.ToString();
     }
 }
